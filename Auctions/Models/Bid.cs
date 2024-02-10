77b17396eff0004f45474b59c0d189f0b375ba18
Joseph Sung <chengyu.sung@gmail.com>
2024-02-10 10:33:25 +0800

    fix indent to 2 on Bid.cs.

--- Auctions/Models/Bid.cs
+++ Auctions/Models/Bid.cs
@@ -4,19 +4,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Auctions.Models
 {
-    public class Bid
-    {
-        public int Id { get; set; }
-        public double Price { get; set; }
+  public class Bid
+  {
+    public int Id { get; set; }
+    public double Price { get; set; }
 
-        [Required]
-        public string? IdentityUserId { get; set; }
-        [ForeignKey("IdentityUserId")]
-        public IdentityUser? User { get; set; }
+    [Required]
+    public string? IdentityUserId { get; set; }
+    [ForeignKey("IdentityUserId")]
+    public IdentityUser? User { get; set; }
 
-        public int? ListingId { get; set; }
-        [ForeignKey("ListingId")]
-        public Listing? Listing { get; set; }
+    public int? ListingId { get; set; }
+    [ForeignKey("ListingId")]
+    public Listing? Listing { get; set; }
 
-    }
+  }
 }
