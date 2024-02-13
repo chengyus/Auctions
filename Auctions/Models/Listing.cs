a67f71d5abc28b2463b9f914f3aa324e113bf01f
Joseph Sung <chengyu.sung@gmail.com>
2024-02-13 19:34:37 +0800

    fix capitalization on the IsSold.

--- Auctions/Models/Listing.cs
+++ Auctions/Models/Listing.cs
@@ -11,7 +11,7 @@ namespace Auctions.Models
     public string Description { get; set; }
     public double Price { get; set; }
     public string ImagePath { get; set; }
-    public bool isSold { get; set; }
+    public bool IsSold { get; set; }
 
     [Required]
     public string? IdentityUserId { get; set; }
